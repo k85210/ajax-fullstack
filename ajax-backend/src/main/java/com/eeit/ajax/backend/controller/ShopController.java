b0546dd8bca02f95e08816f3c9a71605d1fe6f62
k85210 <kl85210@gmail.com>
2026-01-12 13:57:35 +0800

    01:57 前後端分離

--- ajax-backend/src/main/java/com/eeit/ajax/backend/controller/ShopController.java
+++ ajax-backend/src/main/java/com/eeit/ajax/backend/controller/ShopController.java
@@ -6,18 +6,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 @RequestMapping("/shop")
 public class ShopController {
 
+    private static List<String> products = List.of("手機", "手錶", "筆電");
+
+
     @GetMapping("/page")
     public String shopPage(Model model) {
         // 模擬從 model 中取得資料
-        List<String> products = List.of("item1", "item2", "item3");
         model.addAttribute("productList", products);
 
         return "shop_mvc.html";
     }
     
+    @GetMapping("/products")
+    @ResponseBody
+    public List<String> getProducts() {
+        return products;
+    }
+    
+
 }
