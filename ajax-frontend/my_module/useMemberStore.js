315eb32c02e3163e22204688b8d5aefbc4e07a5a
k85210 <kl85210@gmail.com>
2026-01-13 13:36:31 +0800

    0113-3

---
+++ ajax-frontend/my_module/useMemberStore.js
@@ -0,0 +1,11 @@
+
+// const useMemberStore = () => {
+//     // 實作部分等到vue再看
+// }
+
+// export { useMemberStore };
+
+// 上面可縮寫成
+export const useMemberStore = () => {
+    console.log("實作部分等到vue再看")
+}
